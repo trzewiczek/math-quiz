6b12879c87b19e651a5037a47680449d1254ec1d
Krzysztof Trzewiczek <krzyszof.trzewiczek@newvoicemedia.com>
2020-04-10 16:49:33 +0200

    Added: repetitions

--- index.cpp
+++ index.cpp
@@ -4,21 +4,54 @@
 
 using namespace std;
 
+void test_math(int a, int b, int sum);
+
 int main ()
 {
   srand(time(NULL));
 
-  for(int i = 0; i < 30; ++i)
+  for (int i = 0; i < 30; ++i)
   {
-    const int sum = rand() % 5 + 6;  // range [6, 10]
-    const int a   = rand() % sum;    // range [0, sum)
+    const int sum = rand() % 5 + 6;          // range [6, 10]
+    const int a   = rand() % (sum - 1) + 1;  // range [1, sum)
     const int b   = sum - a;
 
+    test_math(a, b, sum);
+  }
+}
+
+void test_math(int a, int b, int sum)
+{
+  int times = a == b ? 1 : 2;
+
+  for (int i = 0; i < times; ++i)
+  {
     int guess = -1;
     while (guess != sum)
     {
       cout << a << " + " << b << " = ";
       cin >> guess;
+    }
+
+    while (guess != b)
+    {
+      cout << sum << " - " << a << " = ";
+      cin >> guess;
+    }
+
+    if (a != b)
+    {
+      while (guess != sum)
+      {
+        cout << b << " + " << a << " = ";
+        cin >> guess;
+      }
+
+      while (guess != a)
+      {
+        cout << sum << " - " << b << " = ";
+        cin >> guess;
+      }
     }
   }
 }
